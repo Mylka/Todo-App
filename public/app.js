f293b45469fb81c7002cbc80bb74fea712718b3d
Marylka <lmary155@gmail.com>
2018-12-27 16:48:47 +0000

    loop through API and list items

--- public/app.js
+++ public/app.js
@@ -1,1 +1,13 @@
-alert("Hi form js!")
+/* global $ */
+$(document).ready(function(){
+    $.getJSON("/api/todos")
+    .then(addTodos)
+});
+
+function addTodos(todos){
+    //add todos to the page
+    todos.forEach(function(todo){
+        var newTodo = $('<li class="task">' + todo.name + '</li>');
+        $('.list').append(newTodo);
+    });
+}
